552ce327b43938be3a8c5ef475a7a9e9f20f54c9
George Anagnostou <anagnostougeorgejohn@gmail.com>
2025-06-02 22:38:55 -0700

    finish ch16 quiz through #3

---
+++ ch16/quiz1.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+int main() {
+    std::vector evens { 2, 4, 6, 8, 10, 12 };
+    
+    const std::vector values { 1.2, 3.4, 5.6, 7.8 };
+    
+    const std::vector<std::string_view> names { "Alex", "Brad", "Charles", "Dave" };
+    
+    std::vector tweleve { 12 };
+    std::vector<int> v(12);
+}
