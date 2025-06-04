06f63b0da29694cc3fb3cee04be52f218def81b1
George Anagnostou <anagnostougeorgejohn@gmail.com>
2025-06-03 18:30:45 -0700

    finish ch16 quiz5.2

--- ch16/c++man.cpp
+++ ch16/c++man.cpp
@@ -15,6 +15,55 @@ namespace WordList {
     }
 }
 
+class Session {
+private:
+    std::string m_word {};
+
+public:
+    Session() : m_word(WordList::getRandomWord()) {}
+
+    // returns the word in plain text
+    const std::string_view getWord() const { return m_word; }
+};
+
+void draw(const Session& s) {
+    std::cout << "The word: ";
+    for ([[maybe_unused]] auto c: s.getWord()) { // step through each letter of word
+        std::cout << '_';
+    } 
+    
+    std::cout << '\n';
+}
+
+char getGuess() {
+    while(true) {
+        std::cout << "Enter your next letter: ";
+
+        char c{};
+        std::cin >> c;
+
+        // try again if bad input
+        if (!std::cin) {
+            // fix it
+            std::cin.clear();
+            std::cout << "That wasn't a valid input. Try again.\n";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        
+        // clear extraneous input
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        // if invalid character, try again
+        if (c < 'a' | c > 'z') {
+            std::cout << "That wasn't a valid input. Try again.\n";
+            continue;
+        }
+        
+        return c;
+    }
+}
+
 void welcome() {
     std::cout << "Welcome to C++man (a variant of Hangman)\n";
     std::cout << "To win: guess the word. To lose: run out of pluses.\n";
@@ -24,7 +73,10 @@ void welcome() {
 int main() {
     welcome();
     
-    std::cout << "The word is: " << WordList::getRandomWord() << "\n";
-
+    Session session {};
+    draw(session);
+    char c { getGuess() };
+    std::cout << "You guessed: " << c << '\n';
+    
     return 0;
 }
