8678f51f007503b8c011af2e49b31fa6efca525d
George Anagnostou <anagnostougeorgejohn@gmail.com>
2025-07-09 22:14:33 -0700

    finish through 21.4

--- ch21/Fraction.cpp
+++ ch21/Fraction.cpp
@@ -7,7 +7,7 @@ private:
 	int m_denomenator { 1 };
 
 public:
-	explicit Fraction(int numerator, int denomenator=1)
+	explicit Fraction(int numerator=0, int denomenator=1)
 		: m_numerator { numerator }
 		, m_denomenator { denomenator }
 		{
@@ -17,6 +17,8 @@ public:
 	friend Fraction operator*(const Fraction& f1, const Fraction& f2);
 	friend Fraction operator*(const Fraction& f1, int x);
 	friend Fraction operator*(int x, const Fraction& f1);
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction& f);
 
 	void reduce() {
 		int gcd { std::gcd(m_numerator, m_denomenator) };
@@ -41,6 +43,28 @@ Fraction operator*(int x, const Fraction& f1) {
 	return Fraction { f1 * x };
 }
 
+std::ostream& operator<<(std::ostream& out, const Fraction& f) {
+	out << f.m_numerator << '/' << f.m_denomenator;
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, Fraction& f) {
+	int numerator{};
+	char symbol {};
+	int denomenator{};
+
+	in >> numerator >> symbol >> denomenator;
+	if (denomenator == 0) {
+		in.setstate(std::ios_base::failbit); // set failure mode manually
+	}
+
+	if (in) {
+		f = Fraction { numerator, denomenator };
+	}
+
+	return in;
+}
+
 int main() {
 	Fraction f1{2, 5};
 	f1.print();
@@ -62,6 +86,18 @@ int main() {
 
 	Fraction f7 { 0, 6 };
 	f7.print();
+
+
+	Fraction f8 {};
+	std::cout << "Enter fraction 1: ";
+	std::cin >> f8;
+
+	Fraction f9 {};
+	std::cout << "Enter fraction 2: ";
+	std::cin >> f9;
+
+	std::cout << f8 << " * " << f9 << " is " << f8 * f9 << '\n';
+
 
 	return 0;
 }
